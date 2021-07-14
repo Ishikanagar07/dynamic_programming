0e3d2e9a0f3c53f5d2949847dbb7d2714a3b5f62
Ishika Nagar <54599671+Ishikanagar07@users.noreply.github.com>
2021-07-14 23:38:56 +0530

    Create canSum_naive_approach.py

---
+++ canSum_naive_approach.py
@@ -0,0 +1,15 @@
+def canSum(targetSum, numbers):
+    if targetSum == 0:
+        return True
+    if targetSum < 0:
+        return False
+    for num in numbers:
+        if canSum(targetSum-num, numbers) == True:
+            return True
+    return False
+
+print(canSum(7, [2,3]))
+print(canSum(7, [5,3,4,7]))
+print(canSum(7, [2,4]))
+print(canSum(8, [2,3,5]))
+#print(camSum(300, [7,14]))
