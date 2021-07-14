67a1afca977ba04212c1265b55ff3dae8e4a58a7
Ishika Nagar <54599671+Ishikanagar07@users.noreply.github.com>
2021-07-15 01:46:56 +0530

    Create howSum_naive_approach.py

---
+++ howSum_naive_approach.py
@@ -0,0 +1,15 @@
+def howSum(targetSum, numbers):
+    if targetSum == 0:
+        return []
+    if targetSum < 0:
+        return None
+
+    for num in numbers:
+        ans = howSum(targetSum-num, numbers)
+        if ans != None:
+            ans.append(num)
+            return ans
+    return None
+
+print(howSum(7,[2,3]))
+print(howSum(7, [5,3,4,7]))
