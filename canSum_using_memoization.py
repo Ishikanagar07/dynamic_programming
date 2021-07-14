592dffe79fa709d51b435ba1e08e2dd3df5f44ef
Ishika Nagar <54599671+Ishikanagar07@users.noreply.github.com>
2021-07-15 01:13:23 +0530

    Create canSum_using_memoization.py

---
+++ canSum_using_memoization.py
@@ -0,0 +1,27 @@
+def canSum(targetSum, numbers, memo = None):
+    if memo == None:
+        memo = {}
+    if targetSum in memo:
+        return memo[targetSum]
+    if targetSum == 0:
+        return True
+    if targetSum < 0:
+        return False
+
+    for num in numbers:
+        remainder = targetSum - num
+        if canSum(remainder, numbers, memo) == True:
+            memo[targetSum] = True
+            return True
+    memo[targetSum] = False
+    return False
+
+
+print(canSum(7, [2,3]))
+print(canSum(7, [5,3,4,7]))
+print(canSum(7, [2,4]))
+print(canSum(8, [2,3,5]))
+print(canSum(300, [7,14]))
+
+# https://florimond.dev/en/posts/2018/08/python-mutable-defaults-are-the-source-of-all-evil/  
+# above link provies the explanation for initializing memo as NONE when passed as argument
