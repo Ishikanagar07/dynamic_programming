73d5f2c96a3653012a626ee761a2f64dc1e9ca3f
Ishika Nagar <54599671+Ishikanagar07@users.noreply.github.com>
2021-07-15 10:20:15 +0530

    Create howSum_using_memoization.py

---
+++ howSum_using_memoization.py
@@ -0,0 +1,22 @@
+def howSum(targetSum, numbers, memo = None):
+    if memo == None:
+        memo = {}
+    if targetSum in memo:
+        return memo[targetSum]
+    if targetSum == 0:
+        return []
+    if targetSum < 0:
+        return None
+
+    for num in numbers:
+        ans = howSum(targetSum-num, numbers, memo)
+        if ans != None:
+            ans = ans + [num]
+            memo[targetSum] = ans
+            return ans
+    memo[targetSum] = None
+    return None
+
+print(howSum(7,[2,3]))
+print(howSum(7, [5,3,4,7]))
+print(howSum(500,[7,8]))
