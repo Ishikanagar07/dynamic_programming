ad777c5ae03a807da5e30a3da6dbc61bb0bc2ac6
Ishika Nagar <54599671+Ishikanagar07@users.noreply.github.com>
2021-07-16 20:53:48 +0530

    Update canSum_using_memoization.py

--- canSum_using_memoization.py
+++ canSum_using_memoization.py
@@ -23,5 +23,11 @@ print(canSum(7, [2,4]))
 print(canSum(8, [2,3,5]))
 print(canSum(300, [7,14]))
 
+
+#Time complexity => O(m*n)
+#Space complexity => O(m)
+
 # https://florimond.dev/en/posts/2018/08/python-mutable-defaults-are-the-source-of-all-evil/  
 # above link provies the explanation for initializing memo as NONE when passed as argument
+
+
