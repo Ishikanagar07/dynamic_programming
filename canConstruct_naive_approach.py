fb9aff1b7fc87c326f55c49e2719c9490b378766
Ishika Nagar <54599671+Ishikanagar07@users.noreply.github.com>
2021-07-17 09:16:58 +0530

    Create canConstruct_naive_approach.py

---
+++ canConstruct_naive_approach.py
@@ -0,0 +1,16 @@
+def canConstruct(target, wordBank):
+    if target == "":
+        return True
+    
+    for word in wordBank:
+        if target.startswith(word):
+            if canConstruct(target[len(word):], wordBank) == True:
+                return True
+    return False
+
+print(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
+print(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
+print(canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]))
+
+#Time complexity => O((n^m)*m)
+#Space complexity => O(m*m) => O(m^2)
